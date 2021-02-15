42895e29fb96d6eec91fdba5c97db1da7cfa06f8
Purachet Ruangtanasak <mosmacprom1@MacBook-Pro-khxng-Purachet.local>
2021-02-15 16:34:15 +0700

    change all horizontal scroll to vertical

--- src/Components/ExperienceComponents/ExperienceListItemTemplate.js
+++ src/Components/ExperienceComponents/ExperienceListItemTemplate.js
@@ -7,10 +7,10 @@ class ExperienceListItemTemplate extends Component {
     }
 
     renderDetails(details, factor) {
-        return(
-            details.map( (detail) => {
-                return(
-                    <p key={detail} style={{ fontSize: 16*factor }}>
+        return (
+            details.map((detail) => {
+                return (
+                    <p key={detail} style={{ fontSize: 16 * factor }}>
                         <span>- </span>
                         <span>{detail}</span>
                     </p>
@@ -23,32 +23,42 @@ class ExperienceListItemTemplate extends Component {
         const { companyName, Duration, workExperience, position, description, details, imageCompanyPath, windowWidth, factor } = this.props;
         const windowWidthHalf = windowWidth / 2;
         return (
-            <div className="ExperienceListItemTemplate">
-                <div className="ExperienceListItemTemplateImage" style={{ width: windowWidthHalf }}>
-                    {companyName && <div className="ExperienceListItemTemplateCompanyFrame" style={{ backgroundImage: `url("/webresume/images/CompanyFrame.png")` }} />}
-                    <div className="ExperienceListItemTemplateLongBlock" />
-                    <p className="ExperienceListItemTemplateDuration">{Duration}</p>
-                    {companyName && <div className="ExperienceListItemTemplateCompanyImage">
-                        <img src={imageCompanyPath} />
-                    </div>}
+            <div className="ExperienceListItemTemplate" >
+                <div className="ExperienceListItemTemplateImage" style={{ width: windowWidth * 0.75, height: 250 * factor }}>
+                    <div className="ExperienceListItemTemplateLongBlock" style={{ height: 20 * factor }} />
+                    <div className="ExperienceListItemTemplateTimeline" />
+                    <div className="ExperienceListItemTemplatePoint" />
+                    <p className="ExperienceListItemTemplateDuration" style={{ fontSize: 16 * factor }}>{Duration}</p>
+                    <div className="ExperienceListItemTemplateCompanyImage" style={{ height: 170 * factor, width: 170 * factor, borderWidth: 12 * factor }}>
+                        <div className="ExperienceListItemTemplateCompanyImage2" style={{ borderWidth: 6*factor }}>
+                            <img src={imageCompanyPath} />
+                        </div>
+                    </div>
                 </div>
-                <div className="ExperienceListItemTemplateTopic">
-                    <p className="ExperienceListItemTemplateCompanyName" style={{ fontSize: 24*factor }}>{companyName}</p>
-                    {workExperience && <p className="ExperienceListItemTemplateWorkExperience" style={{ fontSize: 18*factor }}>[{workExperience}]</p>}
-                </div>
-                <div className="ExperienceListItemTemplateExplanation">
-                    <p style={{ fontSize: 16*factor }}>
-                        <span className="ExperienceListItemTemplateExplanationTitle">Position: </span>
-                        <span>{position}</span>
-                    </p>
-                    <p style={{ fontSize: 16*factor }}>
-                        <span className="ExperienceListItemTemplateExplanationTitle">Description: </span>
-                        <span>{description}</span>
-                    </p>
-                    <p style={{ fontSize: 16*factor }}>
-                        <span className="ExperienceListItemTemplateExplanationTitle">Details: </span>                       
-                    </p>
-                    {this.renderDetails(details, factor)}
+                <div className="ExperienceListItemTemplateTextTimeline">
+                    <div className="ExperienceListItemTemplateTimeline2" />
+                    <div className="ExperienceListItemTemplateText">
+                        <div className="ExperienceListItemTemplateText2" style={{ paddingLeft: 50 * factor, paddingRight: 50 * factor }}>
+                            <div className="ExperienceListItemTemplateTopic">
+                                <p className="ExperienceListItemTemplateCompanyName" style={{ fontSize: 22 * factor }}>{companyName}</p>
+                                <p className="ExperienceListItemTemplateWorkExperience" style={{ fontSize: 18 * factor }}>[{workExperience}]</p>
+                            </div>
+                            <div className="ExperienceListItemTemplateExplanation">
+                                <p style={{ fontSize: 16 * factor }}>
+                                    <span className="ExperienceListItemTemplateExplanationTitle">Position: </span>
+                                    <span>{position}</span>
+                                </p>
+                                <p style={{ fontSize: 16 * factor }}>
+                                    <span className="ExperienceListItemTemplateExplanationTitle">Description: </span>
+                                    <span>{description}</span>
+                                </p>
+                                <p style={{ fontSize: 16 * factor }}>
+                                    <span className="ExperienceListItemTemplateExplanationTitle">Details: </span>
+                                </p>
+                                {this.renderDetails(details, factor)}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- src/Components/SkillComponents/SkillListItemTemplate.js
+++ src/Components/SkillComponents/SkillListItemTemplate.js
@@ -9,10 +9,10 @@ class SkillListItemTemplate extends Component {
     render() {
         const { name, level, imagePath, factor } = this.props;
         return (
-            <div className="SkillListItemTemplate" style={{ width: 400*factor }}>
-                <img src={imagePath} style={{ width: 400*factor }} />
-                <p className="skillName" style={{ fontSize: 24*factor }}>{name}</p>
-                <p className="skillLevel" style={{ fontSize: 18*factor }}>[{level}]</p>
+            <div className="SkillListItemTemplate" style={{ width: 300*factor }}>
+                <img src={imagePath} style={{ width: 300*factor }} />
+                <p className="skillName" style={{ fontSize: 20*factor }}>{name}</p>
+                <p className="skillLevel" style={{ fontSize: 14*factor }}>[{level}]</p>
             </div>
         )
     }
